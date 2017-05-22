11350d564986bd76019019c0f1aab48c32b6c005
Jimmy Charnley Kromann <jimmy@charnley.dk>
2017-05-22 19:48:43 +0200

    no need for codecs

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import setuptools
 from setuptools import setup
 
 # To use a consistent encoding
-from codecs import open
 from os import path
 
 # import version
@@ -14,7 +13,7 @@ from rmsd.calculate_rmsd import __version__
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 short_description = 'Calculate root-mean-square deviation (RMSD), using Kabsch or Quaternion algorithm for rotation, between two Cartesian coordinates in .xyz or .pdb format, resulting in the minimal RMSD.'
