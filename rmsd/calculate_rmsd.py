90f105087129e56eedab06f4451a3769c0b335f8
Jimmy Charnley Kromann <jimmy@charnley.dk>
2022-05-29 16:15:28 +0200

    Added HETATM keyword for PDB reader

--- rmsd/calculate_rmsd.py
+++ rmsd/calculate_rmsd.py
@@ -1448,7 +1448,7 @@ def get_coordinates_pdb(filename, is_gzip=False, return_atoms_as_int=False):
         for line in lines:
             if line.startswith("TER") or line.startswith("END"):
                 break
-            if line.startswith("ATOM"):
+            if line.startswith("ATOM") or line.startswith("HETATM"):
                 tokens = line.split()
                 # Try to get the atomtype
                 try:
